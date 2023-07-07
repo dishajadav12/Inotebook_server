958055fe4938451dd039897fd3d7a0d847d708ec
Disha <dishajadav12402@gmail.com>
2023-07-07 10:43:36 +0530

    setup db & express server

---
+++ models/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    emain:{
+        type: String,
+        required: true,
+        unique: true,
+    },    
+    password:{
+        type: String,
+        required: true,
+    },    
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+});
+module.exports = mongoose.model('User', UserSchema);
